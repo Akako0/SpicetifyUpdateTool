da12eb6e0f7f731428d8fe8c9d47ad830e1e8d19
Akakø <86221868+Akako0@users.noreply.github.com>
2023-04-23 16:26:55 +0200

    Fix 0.0.2

--- update.py
+++ update.py
@@ -3,9 +3,10 @@ import os
 import sys
 import datetime
 
-ROOT = "./Spicetify/"
+ROOT = ""
 CONFIG_PATH = ROOT + "update.conf"
 CACHE_PATH = ROOT + "update.cache"
+
 
 def check_for_update():
     with open(CONFIG_PATH, "r") as config_file:
@@ -18,42 +19,33 @@ def check_for_update():
         key, value = line.split("=", 1)
         if key == "UPDATE_ELAPSE":
             update_elapse = int(value)
-
-    try:
-        update_elapse
-
-    except NameError:
-        print(f"UPDATE_ELAPSE not found in {CONFIG_PATH}.")
-        print("using default settings (once every two days)")
-        update_elapse = 2
+            
 
     for line in cache.splitlines():
         key, value = line.split("=", 1)
         if key == "LAST_UPDATE":
             last_update = value
 
-    try:
-        last_update
 
-    except NameError:
-        print(f"LAST_UPDATE not found in {CACHE_PATH}.")
-        print("using default settings (never)")
-        last_update = None
-
-    if not last_update == None:
-        last_update = datetime.datetime.strptime(last_update, "%m-%d-%Y")
-        last_update = last_update.strftime("%m-%d-%Y")
+    last_update = datetime.datetime.strptime(last_update, "%m-%d-%Y")
+    last_update = last_update.strftime("%m-%d-%Y")
 
     Now = datetime.datetime.now()
 
     minimum_date_for_update = datetime.datetime.strptime(last_update, "%m-%d-%Y")
-    minimum_date_for_update = minimum_date_for_update + datetime.timedelta(days=update_elapse)
+    minimum_date_for_update = minimum_date_for_update + datetime.timedelta(
+        days=update_elapse
+    )
     minimum_date_for_update = minimum_date_for_update.strftime("%m-%d-%Y")
 
-    days_until_update = datetime.datetime.strptime(minimum_date_for_update, "%m-%d-%Y") - Now
+    days_until_update = (
+        datetime.datetime.strptime(minimum_date_for_update, "%m-%d-%Y") - Now
+    )
     days_until_update = days_until_update.days + 1
 
-    print(f"next update will be on {minimum_date_for_update} (in {days_until_update} days)")
+    print(
+        f"next update will be on {minimum_date_for_update} (in {days_until_update} days)"
+    )
 
     if last_update == None or Now.strftime("%m-%d-%Y") >= minimum_date_for_update:
         update_cache("LAST_UPDATE", datetime.datetime.now().strftime("%m-%d-%Y"))
